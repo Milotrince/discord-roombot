9f305b1f0ee655fac61a1cc30f4eec75428cab0c
Milotrince <trinityjchung@gmail.com>
2019-06-25 21:11:12 -0700

    (transfer wip)

--- index.py
+++ index.py
@@ -31,18 +31,46 @@ async def on_ready():
 
 
 @bot.command()
-async def new(ctx):
+async def new(ctx, *args):
     """Make new room"""
+
+    activity =  args[0] if len(args) > 0 else '?'
+    note = args[1] if len(args) > 1 else discord.Embed.Empty
+    waiting_for = args[2] if len(args) > 2 else 2
+
     new_room = dict(
         guild=ctx.message.guild,
-        activity="activity",
-        description="",
+        activity=activity,
+        description=note,
         created=datetime.now(),
         timeout=config['timeout'],
-        players=[ctx.message.author],
-        waiting_for=2
+        players=[ctx.message.author.name],
+        host=ctx.message.author.name,
+        waiting_for=waiting_for
     )
-    rooms.insert(new_room)
+    # rooms.insert(new_room)
+    emb = room_embed(new_room)
+    await ctx.send(embed=emb)
+
+@bot.command()
+async def join(ctx, *args):
+    """Join a room"""
+
+    activity =  args[0] if len(args) > 0 else '?'
+    note = args[1] if len(args) > 1 else discord.Embed.Empty
+    waiting_for = args[2] if len(args) > 2 else 2
+
+    new_room = dict(
+        guild=ctx.message.guild,
+        activity=activity,
+        description=note,
+        created=datetime.now(),
+        timeout=config['timeout'],
+        players=[ctx.message.author.name],
+        host=ctx.message.author.name,
+        waiting_for=waiting_for
+    )
+    # rooms.insert(new_room)
     emb = room_embed(new_room)
     await ctx.send(embed=emb)
 
@@ -55,19 +83,19 @@ async def ping(ctx):
 def room_embed(info):
     embed = discord.Embed(
         color=discord.Color.blue(),
-        description=info.description,
-        timestamp=info.created )
-    embed.set_author(name=info.activity)
+        description=info['description'],
+        timestamp=info['created'] )
+    embed.set_author(name=info['activity'])
     embed.add_field(
-        name="Players ({0})".format(len(info.players)),
-        value=", ".join(info.players) )
+        name="Players ({0})".format(len(info['players'])),
+        value=", ".join(info['players']) )
     embed.add_field(
-        name="Waitng for {0} players".format(info.waiting_for),
-        value="Room will disband in {0} {1}".format(info.timeout, 'seconds') )
+        name="Waitng for {0} players".format(info['waiting_for']),
+        value="Room will disband in {0} {1}".format(info['timeout'], 'seconds') )
     embed.set_footer(
-        text="Host: {0}".format(info.host.name),
-        icon_url=info.host.avatars )
-        
+        text="Host: {0}".format(info['host']),
+        icon_url=discord.Embed.Empty )
+    
     return embed
 
 bot.run(config['token'])
