713cf048afeac0a8e7eb4b7900c1b0a79ac9652d
Milotrince <trinityjchung@gmail.com>
2019-09-10 22:37:04 -0700

    add command: invite
    by request of josejaviergarcia#7777

--- index.py
+++ index.py
@@ -219,6 +219,161 @@ async def join(ctx, *args):
         return await ctx.send("Sorry, no rooms exist yet.")
 
 
+@bot.command(aliases=['i'])
+async def invite(ctx, *args):
+    """Invite a player/players to your room (by name or mention)."""
+    if len(args) < 1:
+        return await ctx.send("Please specify the name or mention of the invitee.")
+    user_mentions = ctx.message.mentions
+    role_mentions = ctx.message.role_mentions
+    player = ctx.message.author
+    invitees = []
+    room = None
+    
+    for invitee in user_mentions:
+        if invitee.id not in invitees:
+            invitees.append(invitee.id)
+
+    for role in role_mentions:
+        for member in role.members:
+            if member.id not in invitees:
+                invitees.append(member.id)
+
+    player_names = []
+    for member in ctx.guild.members:
+        player_names.append(member.display_name.lower())
+        player_names.append(member.name.lower())
+
+    for arg in args:
+        if arg in player_names:
+            p = discord.utils.find(lambda p: p.name.lower() == arg.lower(), ctx.guild.members)
+            if p and p.id not in invitees:
+                invitees.append(p.id)
+
+    rooms_data = rooms.find(guild=ctx.guild.id)
+    room_match = None
+    if rooms_data:
+        for room_data in rooms_data:
+            r = Room.from_query(room_data)
+            if player.id in r.players:
+                room = r
+    if not room:
+        return await ctx.send("You are not in a room.")
+
+    if not invitees:
+        return await ctx.send("Invite who? (Check if the username is correct?)")
+                
+    room.update_active()
+    embed = discord.Embed(
+        color=discord_blue,
+        description="Lucky you.",
+        timestamp=datetime.now(pytz.utc),
+        title=invite_message(player.display_name, room.activity) )
+    embed.add_field(
+        name="Players ({}/{})".format(len(room.players), room.size),
+        value="<@{}>".format(">, <@".join([str(id) for id in room.players])) )
+    embed.add_field(
+        name="Inviter: " + player.display_name,
+        value="Server: " + player.guild.name )
+    embed.add_field(
+        name="Room: " + room.activity,
+        value="Note: " + room.description )
+    embed.add_field(
+        name="Room ID",
+        value=room.role_id )
+    embed.set_footer(
+        text="React to accept or decline the invite.",
+        icon_url=discord.Embed.Empty )
+
+
+    result_embed = discord.Embed(
+        color=discord_blue,
+        description="for room: `{}`".format(room.activity),
+        timestamp=datetime.now(pytz.utc),
+        title="Invites Sent" )
+    result_embed.set_footer(
+        text="Invites sent by: " + player.display_name,
+        icon_url=discord.Embed.Empty )
+    invitee_success = []
+    invitee_fail = []
+    invitee_already_joined = []
+    for invitee_id in invitees:
+        try:
+            if invitee_id in room.players:
+                invitee_already_joined.append(invitee_id)
+                continue
+            invitee = bot.get_user(invitee_id)
+            m = await invitee.send(embed=embed)
+            await m.add_reaction('✅')
+            await m.add_reaction('❌')
+            invitee_list.append(invitee_id)
+            invitee_success.append(invitee_id)
+        except discord.errors.Forbidden as e:
+            invitee_fail.append(invitee_id)
+
+    if invitee_success:
+        result_embed.add_field(
+            name="Invitees",
+            value="<@{}>".format(">, <@".join([str(id) for id in invitee_success])) )
+    if invitee_fail:
+        result_embed.add_field(
+            name="Failed invites",
+            value="I was unable to access these folks, probably because of their settings or because they are a bot.\n<@{}>".format(">, <@".join([str(id) for id in invitee_fail])) )
+    if invitee_already_joined:
+        result_embed.add_field(
+            name="Already joined",
+            value="<@{}>".format(">, <@".join([str(id) for id in invitee_already_joined])) )
+        
+    return await ctx.send(embed=result_embed)
+
+    
+@bot.event
+async def on_reaction_add(reaction, user):
+    player = user
+    channel = reaction.message.channel
+    accept = reaction.emoji == '✅'
+    decline = reaction.emoji == '❌'
+    valid_invite_emoji = accept or decline
+    from_dm = type(channel) is discord.channel.DMChannel
+    if not valid_invite_emoji or player.id not in invitee_list or not from_dm:
+        return
+
+    invitee_list.remove(player.id)
+    room_id = None
+    for field in reaction.message.embeds[0].fields:
+        if field.name == "Room ID":
+            room_id = field.value
+            break
+    room = None
+    room_data = rooms.find_one(role_id=room_id)
+    if room_data:
+        room = Room.from_query(room_data)
+    if not room:
+        return await channel.send("Room no longer exists.")
+
+    if (accept):
+        if player.id in room.players:
+            return await channel.send("You are already in that room.")
+        await channel.send("Invite accepted.")
+        room.update_active()
+        guild = bot.get_guild(room.guild)
+        player = guild.get_member(player.id)
+        if not guild:
+            return await channel.send("I am not in that server anymore :(")
+        if await room.add_player(player):
+            await channel.send(embed=room.get_embed(player, "Room joined by"))
+            room_channel = guild.get_channel(room.channel_id)
+            await room_channel.send(join_message(player.display_name))
+
+            if len(room.players) >= room.size:
+                role = guild.get_role(room.role_id)
+                room_channel = guild.get_channel(room.channel_id)
+                await room_channel.send("Hey {}! {} players have joined.".format(role.mention, len(room.players)))
+    else:
+        await channel.send("Invite declined.")
+            
+
+
 @bot.command(aliases=['x', 'exit', 'disband'])
 async def leave(ctx):
     """Leave a room. If you are the host, the room will be disbanded."""
