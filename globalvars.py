b3f7d4cd8d79e1e0dc4b84473c6633bdcd0116d7
Milotrince <trinityjchung@gmail.com>
2020-03-08 17:55:38 -0700

    update file encoding to utf-8

--- globalvars.py
+++ globalvars.py
@@ -7,5 +7,5 @@ rooms_db = db.get_table('rooms', primary_id='role_id')
 settings_db = db.get_table('settings', primary_id='guild_id')
 
 # Get json file of strings
-with open('config/strings.json', 'r') as strings_file:  
+with open('config/strings.json', 'r', encoding='utf-8') as strings_file:  
     strings = json.load(strings_file)
